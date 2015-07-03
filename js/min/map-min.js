cdad60f0e78e40a977fcfda357a59cd07c06e0ab
Maggie <jfmaggie@gmail.com>
2015-07-02 20:39:31 -0700

    add bootstrap layout with simple google map

---
+++ js/min/map-min.js
@@ -0,0 +1,1 @@
+function initialize(){var a={center:{lat:-34.397,lng:150.644},zoom:8};map=new google.maps.Map(document.getElementById("map-canvas"),a)}var map;google.maps.event.addDomListener(window,"load",initialize);
